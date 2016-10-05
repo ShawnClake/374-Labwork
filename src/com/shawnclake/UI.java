4a12a8a045f4f6705b9e2dc35fb462e11a2859f0
Shawn <shawn.clake@gmail.com>
2016-10-05 16:19:39 -0600

    Added UI class

---
+++ src/com/shawnclake/UI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shawnclake;
+
+/**
+ * @author sseStudent_2
+ *
+ */
+public class UI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
