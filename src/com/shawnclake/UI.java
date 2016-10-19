d2e528508df819f40c5afadc69815e3385256e6c
ShawnClake <shawn.clake@gmail.com>
2016-10-19 09:35:49 -0600

    Started adding the UI

--- src/com/shawnclake/UI.java
+++ src/com/shawnclake/UI.java
@@ -2,7 +2,7 @@
  * 
  */
 package com.shawnclake;
-
+import java.util.Scanner;
 /**
  * @author sseStudent_2
  *
@@ -14,6 +14,49 @@ public class UI {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		LinkedList list = new LinkedList();
+		
+		System.out.println("Doubly Linked List");
+		
+		boolean running = true;
+		
+		while(running)
+		{
+			printMenu();
+			
+			String input = in.nextLine();
+			
+			if(input.equals("q")) {
+				running = false;				
+			}
+			
+			if(input.equals("1"))
+			{
+				int data = 0;
+				System.out.println("Enter the data for the element (int): ");
+				data = in.nextInt();
+				
+				ListElement le = new ListElement();
+				le.setData(data);
+				
+				list.addElement(le);
+			}
+			
+			if(input.equals("2"))
+			{
+				int index = 0;
+				System.out.println("Enter the index to delete (int): ");
+				index = in.nextInt();
+				
+				ListElement le = list.deleteElement(index);
+			}
+			
+			
+		}
+		
+		
 		ListElement le = new ListElement();
 		le.setData(5);
 		
@@ -23,7 +66,7 @@ public class UI {
 		ListElement le4 = new ListElement();
 		le4.setData(7);
 		
-		LinkedList list = new LinkedList();
+		
 		
 		list.addElement(le);
 		list.addElement(le2);
@@ -32,6 +75,14 @@ public class UI {
 		
 		list.printLinkedListHead();
 		
+	}
+	
+	public static void printMenu() {
+		System.out.println("Enter 1 to add an element");
+		System.out.println("Enter 2 to delete an element");
+		System.out.println("Enter 3 to print from head");
+		System.out.println("Enter 4 to print from tail");
+		System.out.println("Enter q to exit");
 	}
 
 }
