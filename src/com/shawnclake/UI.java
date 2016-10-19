f51baab2ef16239bac53a59a86ed894aad0550e1
ShawnClake <shawn.clake@gmail.com>
2016-10-19 09:38:19 -0600

    Finished UI and tested. Works.

--- src/com/shawnclake/UI.java
+++ src/com/shawnclake/UI.java
@@ -53,27 +53,17 @@ public class UI {
 				ListElement le = list.deleteElement(index);
 			}
 			
+			if(input.equals("3"))
+			{
+				list.printLinkedListHead();
+			}
+			
+			if(input.equals("4"))
+			{
+				list.printLinkedListTail();
+			}
 			
 		}
-		
-		
-		ListElement le = new ListElement();
-		le.setData(5);
-		
-		ListElement le2 = new ListElement();
-		le2.setData(10);
-		
-		ListElement le4 = new ListElement();
-		le4.setData(7);
-		
-		
-		
-		list.addElement(le);
-		list.addElement(le2);
-		list.addElement(le4);
-		ListElement le3 = list.deleteElement(0);
-		
-		list.printLinkedListHead();
 		
 	}
 	
